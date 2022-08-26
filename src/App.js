b433ba3f6ddaa2e86b4deed0b0b2fa884c0c94db
airhop <airhop12@gmail.com>
2022-08-26 16:39:01 -0600

    fix: fixing settings dialog and adding dark mode

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Dashboard from './Dashboard';
-import GuessInput from './GuessInput';
 import React from 'react';
 
 function App() {
--- src/dialogs/settings.js
+++ src/dialogs/settings.js
@@ -1,26 +1,25 @@
 import * as React from 'react';
 import { useReactiveVar } from '@apollo/client';
-import { settingsDialogOpen } from '../reactive-vars';
+import { settingsDialogOpen, darkMode } from '../reactive-vars';
 import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
 import Dialog from '@mui/material/Dialog';
 import Typography from '@mui/material/Typography';
 import Switch from '@mui/material/Switch';
-import Button from '@mui/material/Button';
-import Avatar from '@mui/material/Avatar';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Dialog from '@mui/material/Dialog';
-import PersonIcon from '@mui/icons-material/Person';
-import AddIcon from '@mui/icons-material/Add';
-import Typography from '@mui/material/Typography';
-ListItem
 
-const ToggleOption = ({title, onToggle}) => {
-
+const ToggleOption = ({title, on, toggle}) => {
+  return (    
+    <ListItem>
+      <ListItemText primary={title} />
+      <Switch
+        edge="end"
+        onChange={() => toggle(!on)}
+        checked={on}
+      />
+    </ListItem>
+  )
 }
 
 
@@ -28,6 +27,7 @@ const ToggleOption = ({title, onToggle}) => {
 
 export const SettingsDialog = () => {
   const open = useReactiveVar(settingsDialogOpen);
+  const darkModeOn = useReactiveVar(darkMode);
 
   const handleClose = () => {
     settingsDialogOpen(false);
@@ -37,22 +37,7 @@ export const SettingsDialog = () => {
     <Dialog maxWidth="md" onClose={handleClose} open={open}>
       <DialogTitle>Settings</DialogTitle>
       <List sx={{ pt: 0 }}>
-        <ListItem>
-          <ListItemAvatar>
-            <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
-              <PersonIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary={email} />
-        </ListItem>
-        <ListItem autoFocus button onClick={() => handleListItemClick('addAccount')}>
-          <ListItemAvatar>
-            <Avatar>
-              <AddIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Add account" />
-        </ListItem>
+        <ToggleOption title={"Dark Mode"} on={darkModeOn} toggle={darkMode} />
       </List>
     </Dialog>
   );
