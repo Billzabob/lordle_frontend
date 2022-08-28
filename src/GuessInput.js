4fe14fe68ca67ee920489764b1fc9d7acf513847
Billzabob <cocolymoo@gmail.com>
2022-08-27 22:46:58 -0600

    Alphabetize

--- src/GuessInput.js
+++ src/GuessInput.js
@@ -14,12 +14,13 @@ const GET_CARDS = gql`
 export default function GuessInput() {
   const { data } = useQuery(GET_CARDS);
   const cards = data?.cards || []
+  const sorted = cards.slice().sort((c1, c2) => c1.name > c2.name ? 1 : -1)
 
   return <Autocomplete
     autoComplete={true}
     filterOptions={createFilterOptions({limit: 10})}
     id="guess-input"
-    options={cards}
+    options={sorted}
     onChange={(_e, {cardCode}) => console.log(cardCode)}
     sx={{ width: 300 }}
     getOptionLabel={(option) => option.name}
