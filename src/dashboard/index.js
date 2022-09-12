24c0adea114f6796b2bd50853f7aff181b7e0fd1
Billzabob <cocolymoo@gmail.com>
2022-09-12 16:53:24 -0600

    Add logo

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -1,30 +1,30 @@
-import * as React from 'react'
-import { useReactiveVar } from '@apollo/client'
 import { createTheme, ThemeProvider, responsiveFontSizes } from '@mui/material/styles'
+import { darkMode, settingsDialogOpen, statsDialogOpen } from '../reactive-vars'
+import { mainListItems } from './list-items'
+import { SettingsDialog, StatsChartDialog } from '../dialogs'
+import { useReactiveVar } from '@apollo/client'
+import * as React from 'react'
+import AppBar from './app-bar'
+import BarChartIcon from '@mui/icons-material/BarChart'
 import Box from '@mui/material/Box'
-import Drawer from '@mui/material/Drawer'
-import CssBaseline from '@mui/material/CssBaseline'
-import Toolbar from '@mui/material/Toolbar'
-import List from '@mui/material/List'
-import Typography from '@mui/material/Typography'
-import Divider from '@mui/material/Divider'
-import IconButton from '@mui/material/IconButton'
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
 import ChevronRightIcon from '@mui/icons-material/ChevronRight'
-import MenuIcon from '@mui/icons-material/Menu'
-import SettingsIcon from '@mui/icons-material/Settings'
-import BarChartIcon from '@mui/icons-material/BarChart'
-import { SettingsDialog, StatsChartDialog } from '../dialogs'
-import { mainListItems } from './list-items'
-import { darkMode, settingsDialogOpen, statsDialogOpen } from '../reactive-vars'
-import AppBar from './app-bar'
+import Countdown from './countdown'
+import CssBaseline from '@mui/material/CssBaseline'
+import Divider from '@mui/material/Divider'
+import Drawer from '@mui/material/Drawer'
 import DrawerHeader from './drawer-header'
 import GuessCounter from './guess-counter'
+import GuessingGame from '../guessing-game/GuessingGame'
+import IconButton from '@mui/material/IconButton'
+import List from '@mui/material/List'
+import Logo from '../Logo'
 import Main from './main'
 import MainGameFooter from './main-game-footer'
 import MainGameTitle from './main-game-title'
-import GuessingGame from '../guessing-game/GuessingGame'
-import Countdown from './countdown'
+import MenuIcon from '@mui/icons-material/Menu'
+import SettingsIcon from '@mui/icons-material/Settings'
+import Toolbar from '@mui/material/Toolbar'
 
 const drawerWidth = 240
 
@@ -63,14 +63,11 @@ function DashboardContent() {
             >
               <MenuIcon />
             </IconButton>
-            <Typography
-              component='h1'
-              variant='h5'
-              color='inherit'
-              noWrap
-              sx={{ flexGrow: 1 }}>
-              LoRdle
-            </Typography>
+            <Box mt={1} sx={{ flexGrow: 1 }}>
+              <a href='https://www.lordle.gg'>
+                <Logo/>
+              </a>
+            </Box>
             <IconButton color='inherit' sx={{ mr: 1 }} onClick={() => statsDialogOpen(true)}>
               <BarChartIcon />
             </IconButton>
