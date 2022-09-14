335300a0d505bcd6d602e0f5a0b5c1592f66c0bc
Billzabob <cocolymoo@gmail.com>
2022-09-14 10:57:31 -0600

    Code cleanup

--- src/dashboard/app-bar.js
+++ src/dashboard/app-bar.js
@@ -7,39 +7,48 @@ import { settingsDialogOpen } from '../reactive-vars'
 import { drawerOpen } from './drawer'
 import pages from './pages'
 
+const smallLayout = (
+  <>
+    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+      <IconButton
+        size='large'
+        onClick={() => drawerOpen(true)}
+        color='inherit'
+      >
+        <MenuIcon />
+      </IconButton>
+    </Box>
+
+    <Logo sx={{ display: { xs: 'flex', md: 'none', flexGrow: 1 } }} />
+  </>
+)
+
+const largeLayout = (
+  <>
+    <Logo sx={{ display: { xs: 'none', md: 'flex' }, ml: 3 }} />
+
+    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, ml: 3 }}>
+      {pages.map((page) => (
+        <Button
+          key={page}
+          sx={{ mx: 1, color: 'white', display: 'block' }}
+        >
+          <Typography variant='h6'>
+            {page}
+          </Typography>
+        </Button>
+      ))}
+    </Box>
+  </>
+)
+
 export default function MyAppBar() {
   return (
     <AppBar component='nav'>
       <Box mx={3}>
         <Toolbar disableGutters>
-          <Logo sx={{ display: { xs: 'none', md: 'flex' }, ml: 3 }}/>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size='large'
-              onClick={() => drawerOpen(true)}
-              color='inherit'
-            >
-              <MenuIcon/>
-            </IconButton>
-          </Box>
-
-          <Logo sx={{ display: { xs: 'flex', md: 'none', flexGrow: 1 }}}/>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, ml: 3 }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                // onClick={handleCloseNavMenu}
-                sx={{ mx: 1, color: 'white', display: 'block' }}
-              >
-                <Typography variant='h6'>
-                  {page}
-                </Typography>
-              </Button>
-            ))}
-          </Box>
-
+          {smallLayout}
+          {largeLayout}
           <Box sx={{ flexGrow: 0 }}>
             <IconButton color='inherit' onClick={() => settingsDialogOpen(true)}>
               <SettingsIcon />
