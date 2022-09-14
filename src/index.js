616a53315d43379605e6f135255ebf55f206cd73
Billzabob <cocolymoo@gmail.com>
2022-09-13 22:00:59 -0600

    Try to fix weird font issue

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
 import Dashboard from './dashboard'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
