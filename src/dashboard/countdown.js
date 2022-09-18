84af179269b02afd51d5d5c63593d0c1e9205184
Billzabob <cocolymoo@gmail.com>
2022-09-18 10:27:21 -0600

    Add refresh button when countdown hits 0

--- src/dashboard/countdown.js
+++ src/dashboard/countdown.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Box, Skeleton, Typography } from '@mui/material'
+import { Box, Button, Skeleton, Typography } from '@mui/material'
 import { useQuery } from '@apollo/client'
 import { COUNTDOWN_QUERY } from '../gql/queries'
 
@@ -7,7 +7,7 @@ export default function Countdown() {
   const { data, loading } = useQuery(COUNTDOWN_QUERY)
   const [timeLeft, setTimeLeft] = useState(null)
 
-  if (!timeLeft && data) setTimeLeft(data.nextCardTimeSeconds)
+  timeLeft === null && data && setTimeLeft(data.nextCardTimeSeconds)
 
   useEffect(() => {
     const timer = timeLeft > 0 && setTimeout(() => setTimeLeft(t => t - 1), 1000)
@@ -15,13 +15,17 @@ export default function Countdown() {
   })
 
   // This could be much cleaner with a time library
-  const timeString = timeLeft ? (new Date(timeLeft * 1000)).toISOString().substring(11, 19) : ''
+  const timeString = (new Date(timeLeft * 1000)).toISOString().substring(11, 19)
 
   return (
     <Box display='flex' justifyContent='center'>
-      <Typography variant='h6' sx={{ mt: 3}}>
-        {loading ? <Skeleton width={400}/> : 'Next challenge in: ' + timeString}
-      </Typography>
+      {
+        timeLeft === 0
+          ? <Button href='/' sx={{ mt: 2.5 }} variant='contained'>Refresh</Button>
+          : <Typography variant='h6' sx={{ mt: 3 }}>
+            {loading ? <Skeleton width={400} /> : 'Next challenge in: ' + timeString}
+          </Typography>
+      }
     </Box>
   )
 }
