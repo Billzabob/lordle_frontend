512e5eebdc8287d02ec3920008fd0582e5f20da7
Billzabob <cocolymoo@gmail.com>
2022-09-21 15:23:07 -0600

    Soften the glow on button hover

--- src/dashboard/styled-components.js
+++ src/dashboard/styled-components.js
@@ -2,7 +2,7 @@ import { Button, styled } from "@mui/material";
 
 export const StyledButton = styled(Button)(({theme}) => ({
   '&:hover': {
-    backgroundImage: 'radial-gradient(#41494E 40%, #A8F8FE 100%)',
+    backgroundImage: 'radial-gradient(#362E2B 50%, #A8F8FE 150%)',
   },
   color: theme.palette.secondary.main,
   fontWeight: 'bold'
