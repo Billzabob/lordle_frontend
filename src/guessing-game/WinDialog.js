6678ffeb175190e2d09b8c38d39c5b0ee06ee7f8
Billzabob <cocolymoo@gmail.com>
2022-09-25 10:25:38 -0600

    Set columns to always 3

--- src/guessing-game/WinDialog.js
+++ src/guessing-game/WinDialog.js
@@ -36,7 +36,7 @@ export default function WinDialog({ results }) {
               <Typography variant='h4' sx={{ textAlign: 'center', mt: 0 }}>
                 Other Possible Answers
               </Typography>
-              <ImageList cols={Math.min(3, correctCard?.otherCards.length)} sx={{ m: 1 }}>
+              <ImageList cols={3} sx={{ m: 1 }}>
                 {correctCard?.otherCards.map((item) => (
                   <CardTooltip key={item.name} image={item.image} name={item.name}>
                     <ImageListItem>
