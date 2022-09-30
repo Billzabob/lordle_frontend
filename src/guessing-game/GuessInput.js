8ab6cb1695446506ec2e38b543fcec9ea98f6fe5
Billzabob <cocolymoo@gmail.com>
2022-09-30 11:30:41 -0600

    Make guess input less transparent

--- src/guessing-game/GuessInput.js
+++ src/guessing-game/GuessInput.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Autocomplete, createFilterOptions, Stack, TextField } from '@mui/material'
+import { Autocomplete, createFilterOptions, Stack, TextField, useTheme } from '@mui/material'
 import { useQuery } from '@apollo/client'
 import { GET_CARDS } from '../gql/queries'
 
@@ -8,6 +8,7 @@ export default function GuessInput({ setGuess, guesses }) {
   const [input, setInput] = useState('')
   const cards = data?.cards || []
   const sorted = cards.slice().sort((c1, c2) => c1.name > c2.name ? 1 : -1)
+  const theme = useTheme()
 
   return (
     <Stack direction='row' justifyContent='center' sx={{ mb: 2, mt: 4.5 }}>
@@ -23,7 +24,12 @@ export default function GuessInput({ setGuess, guesses }) {
           if (c) setGuess(c.cardCode)
           setInput('')
         }}
-        sx={{ width: 300 }}
+        sx={{
+          width: 300,
+          '& .MuiInputBase-root': {
+            backgroundColor: theme.palette.mode === 'dark' ? '#00000088' : '#FFFFFF88',
+          },
+        }}
         getOptionLabel={(option) => option.name}
         inputValue={input}
         renderInput={(params) => <TextField onChange={(e) => setInput(e.target.value)} {...params} label='Guess' />}
