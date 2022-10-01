c82e5378bf21df5e253b28ad4914b8e7b7eb323a
Billzabob <cocolymoo@gmail.com>
2022-10-01 11:08:29 -0600

    Fix button highlight

--- src/dashboard/styled-components.js
+++ src/dashboard/styled-components.js
@@ -2,7 +2,7 @@ import { Button, styled } from '@mui/material'
 
 export const StyledButton = styled(Button)(({theme}) => ({
   '&:hover': {
-    backgroundImage: 'radial-gradient(#362E2B 50%, #A8F8FE 150%)',
+    backgroundImage: 'radial-gradient(#C2A052 50%, #A8F8FE 150%)',
   },
   fontWeight: 'bold'
 }))
