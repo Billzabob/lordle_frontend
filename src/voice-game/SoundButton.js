2b3a27bbf60345c647226e1888d03c2db01b32c7
Billzabob <cocolymoo@gmail.com>
2022-10-10 15:27:05 -0600

    Pause sound button

--- src/voice-game/SoundButton.js
+++ src/voice-game/SoundButton.js
@@ -2,27 +2,49 @@ import { Box, IconButton, Skeleton } from "@mui/material";
 import { CURRENT_DAY, GET_VOICE_LINES } from "../gql/queries";
 import { useQuery } from "@apollo/client";
 import PlayCircleFilledTwoToneIcon from '@mui/icons-material/PlayCircleFilledTwoTone'
-import React, { useState } from 'react'
+import PauseCircleFilledTwoToneIcon from '@mui/icons-material/PauseCircleFilledTwoTone'
+import React, { useEffect, useState } from 'react'
 
 export default function SoundButton() {
   const dayQuery = useQuery(CURRENT_DAY)
   const currentDay = dayQuery.data.currentDay.day
   const { data, loading } = useQuery(GET_VOICE_LINES, { variables: { day: currentDay } })
   const [playCount, setPlayCount] = useState(0)
+  const [playing, setPlaying] = useState(false)
+  const [audio, setAudio] = useState(new Audio())
+  const voiceLines = data?.voiceLines
 
-  const audio = new Audio(data?.voiceLines[playCount % data?.voiceLines.length])
+  useEffect(() => setAudio(new Audio(voiceLines[playCount % voiceLines.length])), [voiceLines, playCount])
+
+  useEffect(() => {
+    audio.addEventListener('ended', () => {
+      setPlaying(false)
+      setPlayCount(c => c + 1)
+    })
+  })
 
   const playSound = () => {
+    setPlaying(true)
     audio.play()
+  }
+
+  const pauseSound = () => {
+    setPlaying(false)
+    audio.pause()
     setPlayCount(c => c + 1)
   }
 
   return (
     <Box display='flex' justifyContent='center'>
       {loading ? <Skeleton variant='rectangular' width={125} height={125} /> :
-        <IconButton color='primary' onClick={playSound}>
-          <PlayCircleFilledTwoToneIcon style={{ fontSize: 120 }} />
-        </IconButton>
+        playing ?
+          <IconButton color='primary' onClick={pauseSound}>
+            <PauseCircleFilledTwoToneIcon style={{ fontSize: 120 }} />
+          </IconButton>
+          :
+          <IconButton color='primary' onClick={playSound}>
+            <PlayCircleFilledTwoToneIcon style={{ fontSize: 120 }} />
+          </IconButton>
       }
     </Box>
   )
