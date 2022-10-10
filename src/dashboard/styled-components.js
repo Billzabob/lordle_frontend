5e904d1e501a65c880d6e8625cce2818bcb5eddc
Billzabob <cocolymoo@gmail.com>
2022-10-10 12:18:26 -0600

    Fix hover on app bar

--- src/dashboard/styled-components.js
+++ src/dashboard/styled-components.js
@@ -3,6 +3,7 @@ import { Button, styled } from '@mui/material'
 export const StyledButton = styled(Button)(({theme}) => ({
   '&:hover': {
     backgroundImage: 'radial-gradient(#C2A052 50%, #A8F8FE 150%)',
+    color: 'black'
   },
   fontWeight: 'bold'
 }))
