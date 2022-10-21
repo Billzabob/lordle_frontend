ae6e57b3073f5b5ccfe9f9a7d75f7dd91b95a29a
Billzabob <Billzabob@users.noreply.github.com>
2022-10-21 08:00:28 -0500

    Use Language type

--- src/gql/queries/voice-guess.js
+++ src/gql/queries/voice-guess.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client'
 
 export const CHECK_VOICE_GUESS = gql`
-  query VoiceLineGuess($code: String!, $day: Int!, $language: String) {
+  query VoiceLineGuess($code: String!, $day: Int!, $language: Language) {
     guessVoice(code: $code, day: $day, language: $language) {
       cardCode
       correct
